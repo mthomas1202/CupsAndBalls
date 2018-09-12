1cedbbc7551f612c41787ab6a76b7802da97ffe0
Matthew Thomas <matt.thomas1202@yahoo.com>
2018-09-12 18:53:11 -0400

    raise and lower cups and move ball under 1 cup logic

---
+++ Assets/Cup.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cup : MonoBehaviour {
+
+    public float downHeight = 1.3f;
+    public float upHeight = 1.46f;
+    public float movingSpeed = 3f;
+
+    public GameObject ball;
+
+    private Vector3 targetPosition;
+	// Use this for initialization
+	void Start () {
+
+        targetPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * movingSpeed);
+	}
+
+    public void MoveUp()
+    {
+        targetPosition = new Vector3(
+           transform.position.x,
+           upHeight,
+           transform.position.z
+           );
+    }
+
+    public void MoveDown()
+    {
+        targetPosition = new Vector3(
+           transform.position.x,
+           downHeight,
+           transform.position.z
+           );
+    }
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject ball;
+    public Cup[] cups;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(ShuffleRoutine());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private IEnumerator ShuffleRoutine()
+    {
+        yield return new WaitForSeconds(1f);
+
+        foreach (Cup cup in cups)
+        {
+            cup.MoveUp();
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        Cup targetCup = cups[Random.Range(0, cups.Length)];
+        targetCup.ball = ball;
+        ball.transform.position = new Vector3(
+            targetCup.transform.position.x,
+            ball.transform.position.y,
+            targetCup.transform.position.z);
+
+        yield return new WaitForSeconds(1.0f);
+
+        foreach (Cup cup in cups)
+        {
+            cup.MoveDown();
+        }
+
+    }
+}
